28af00ccaa8121ced9293cc5139441d1d21a5f1b
Tassio <tassio.tristao9@gmail.com>
2024-03-18 12:13:47 -0300

    Initial Commit

---
+++ List_1/src/main/java/Exercise_1.java
@@ -0,0 +1,19 @@
+// Celsius to Fahrenheit Converter
+
+import java.util.Scanner;
+
+public class Exercise_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user for temperature in Celsius
+        System.out.println("Enter temperature in Celsius: ");
+        float celsius = input.nextFloat();
+
+        // Convert Celsius for Fahrenheit
+        double fahrenheit = celsius * (9.0 / 5.0) + 32;
+
+        // Result in Fahrenheit
+        System.out.println("Temperature in Fahrenheit " + fahrenheit);
+    }
+}

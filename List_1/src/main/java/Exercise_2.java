65d8b6d376f229958ea8dd42e6691173c4bd33d7
Tassio <tassio.tristao9@gmail.com>
2024-03-18 12:21:09 -0300

    Exercise 2

---
+++ List_1/src/main/java/Exercise_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Exercise_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user for speed in km/h
+        System.out.println("Enter speed in km/h:");
+        double kmh = scanner.nextDouble();
+
+        // Convert km/h to m/s
+        double ms = kmh / 3.6;
+
+        // Display result in m/s
+        System.out.println("Speed in m/s: " + ms);
+    }
+}

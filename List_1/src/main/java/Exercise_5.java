76be6ae56f852a797b5ec36db665f32ecaadf8f5
Tassio <tassio.tristao9@gmail.com>
2024-03-21 10:21:46 -0300

    Write reversed a integer

---
+++ List_1/src/main/java/Exercise_5.java
@@ -0,0 +1,27 @@
+/** Write a program that reads a positive three-digit integer (from 100 to 999)
+Generate another number formed by the reversed digits of the input number.
+*/
+
+import java.util.Scanner;
+
+public class Exercise_5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int originalNumber;
+
+        // Get the integer from the user
+        System.out.println("Enter integer of three digits: ");
+        originalNumber = input.nextInt();
+
+        int unit, ten, hundred, reversedNumber;
+
+        unit = originalNumber % 10;
+        ten = (originalNumber / 10) % 10;
+        hundred = originalNumber / 100;
+
+        reversedNumber = unit * 100 + ten * 10 + hundred;
+
+        System.out.println("Inverted number: " + reversedNumber);
+    }
+}
